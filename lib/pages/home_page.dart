9e8254ea06c35dbc8ec0eac0733655a675931425
doguk <dogukan.hacifazlioglu@bil.omu.edu.tr>
2022-05-18 21:47:52 +0300

    create navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,23 +11,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  int _selectedIndex = 0;
+
+  PageController pageController = PageController();
+
+  void onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.jumpToPage(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Signed in as: ' + user.email!),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.deepPurple,
-              child: Text('çıkış'),
+      body: PageView(
+        controller: pageController,
+        children: [
+          Container(
+            child: Center(
+              child: Text('HomePage'),
             ),
-          ],
-        ),
+          ),
+          Container(
+            child: Center(
+              child: Text('Camera'),
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Giriş yapılan email ${user.email}'),
+                MaterialButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  color: Colors.deepPurple,
+                  child: Text('Çıkış yap'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: 'Camera',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: onTapped,
       ),
     );
   }
