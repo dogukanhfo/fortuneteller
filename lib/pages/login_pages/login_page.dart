353283c8e579997367b6e97930ef87ea0b99e3a1
doguk <dogukan.hacifazlioglu@bil.omu.edu.tr>
2022-06-15 16:43:57 +0300

    update auth to google_auth

--- lib/pages/login_pages/login_page.dart
+++ lib/pages/login_pages/login_page.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fortuneteller/consts.dart';
-import 'package:fortuneteller/pages/navbar_pages/profilepage/create_profile.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:fortuneteller/pages/login_pages/google_sign_in_btn.dart';
+import 'package:fortuneteller/services/auth.dart';
+import 'package:fortuneteller/services/auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,145 +12,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  //text controllers
-
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: _emailController.text.trim(),
-      password: _passwordController.text.trim(),
-    );
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(BACKGROUND_COLOR),
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //logo
-
-                Icon(
-                  Icons.coffee,
-                  size: 100,
-                  color: Color(ICON_COLOR),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-
-                Text(
-                  APP_NAME,
-                  style: GoogleFonts.poppins(
-                    fontSize: 52,
-                  ),
-                ),
-
-                Text(
-                  'Hoşgeldin, hadi fal bakalım',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-
-                //email textfield
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color(TEXT_FIELD_BORDER_COLOR)),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color(CLICKED_TEXT_FIELD_BORDER_COLOR)),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      hintText: 'email',
-                      fillColor: Color(TEXT_FIELD_COLOR),
-                      filled: true,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-
-                //password textfield
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color(TEXT_FIELD_BORDER_COLOR)),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color(CLICKED_TEXT_FIELD_BORDER_COLOR)),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      hintText: 'password',
-                      fillColor: Color(TEXT_FIELD_COLOR),
-                      filled: true,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-
-                //Login Button
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: signIn,
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Color(BUTTON_COLOR),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Giriş yap',
-                          style: TextStyle(
-                            color: Color(BUTTON_TEXT_COLOR),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Flexible(
+                      flex: 1,
+                      child: Icon(
+                        Icons.coffee,
+                        size: 80,
+                        color: Color(TEXT_COLOR),
                       ),
                     ),
-                  ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Falmatik',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              GoogleSignInButton(),
+            ],
           ),
         ),
       ),
